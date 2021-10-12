f05d8e82c09abc760c0bfee9bf249a443233f798
juicejeff19 <jalcalar1900@alumno.ipn.mx>
2021-10-12 03:15:41 -0500

    porfavor funciona 2

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.set('view engine', 'ejs')
  */ 
 var index = require('./routes/index')
 var users = require('./routes/users')
+app.set('port', process.env.PORT || 5000);
 
 
 /**
@@ -112,7 +113,7 @@ app.use('/users', users)
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3000, function(){
-	console.log('Server running at port 3000: http://127.0.0.1:3000')
-})
+app.listen(app.get('port'), () => {
+	console.log(`Servidor escuchando desde el puerto  ${app.get('port')}`);
+});
 
